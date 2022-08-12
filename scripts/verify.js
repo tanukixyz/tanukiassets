7c3d7bda639f20a6797dac69aad299b384665f35
Noat <noat.eth@gmail.com>
2022-08-12 18:04:19 +0700

    format token lists

--- scripts/verify.js
+++ scripts/verify.js
@@ -3,16 +3,20 @@
 const fs = require('fs');
 
 const tokenListsPath = './tokenlists';
+const tanukiList = 'tanuki-default.json';
 
 (async function () {
   // verify and sort token lists data
   const lists = fs.readdirSync(tokenListsPath);
 
+  let allTokens = [];
+
   for (const list of lists) {
+    if (list === tanukiList) continue;
+
     let data = JSON.parse(fs.readFileSync(`${tokenListsPath}/${list}`).toString());
 
     console.assert(data.name !== undefined, 'token list must has a name');
-    console.assert(data.version !== undefined, 'token list must has a version');
     console.assert(data.tokens !== undefined && data.tokens.length, 'token list must has token arrays');
 
     for (const token of data.tokens) {
@@ -21,6 +25,26 @@ const tokenListsPath = './tokenlists';
       console.assert(token.decimals !== undefined, 'token must has a decimals');
       console.assert(token.logoURI !== undefined, 'token must has a logo uri');
       console.assert(token.addresses !== undefined, 'token must has addresses');
+
+      // we merge all tokens into single list
+      function findDupToken(token, tokens) {
+        for (let i = 0; i < tokens.length; i++) {
+          if (token.symbol === tokens[i].symbol) {
+            return i;
+          }
+        }
+
+        return -1;
+      }
+
+      const index = findDupToken(token, allTokens);
+      if (index === -1) {
+        allTokens.push(token);
+      } else {
+        for (const [chain, address] of Object.entries(token.addresses)) {
+          allTokens[index].addresses[chain] = address;
+        }
+      }
     }
 
     // sort token list by name
@@ -31,4 +55,18 @@ const tokenListsPath = './tokenlists';
     // save sorted data
     fs.writeFileSync(`${tokenListsPath}/${list}`, JSON.stringify(data).toString());
   }
+
+  // sort token list by name
+  allTokens = allTokens.sort(function (a, b) {
+    return a.name > b.name ? 1 : -1;
+  });
+
+  // save single list
+  fs.writeFileSync(
+    `${tokenListsPath}/tanuki-default.json`,
+    JSON.stringify({
+      name: 'tanuki',
+      tokens: allTokens,
+    }).toString()
+  );
 })();
